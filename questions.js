ac7a687b368cafe4f18ef6b6aabb9dbf637e2069
Aaron Boone <37162732+aboone2000@users.noreply.github.com>
2022-10-23 13:59:11 -0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,49 @@
+
+const questions = [
+    {
+        question: 'Who won MVP (Most Valuable Player)in the 2020 NBA Finals?',
+        answers: [
+            { Text: 'Lebron James', correct: true },
+            { Text: 'Anthony Davis', correct: false },
+            { Text: 'Jimmy Butler', correct: false },
+            { Text: 'Jayson Tatum', correct: false }
+
+        ]
+    },
+    {
+        question: 'What two teams faced off in the NBA Finals in 2016?',
+        answers: [
+            { Text: 'OKC Thunder Vs Toronto Raptors', correct: false },
+            { Text: 'San Antonio Spurs Vs Miami Heat', correct: false },
+            { Text: 'Golden State Warriors Vs Cleveland Cavilers', correct: true },
+            { Text: 'LA Lakers Vs Boston Celtics', correct: false },
+        ]
+    },
+    {
+        question: 'Who won MVP in the 2016/2017 regular season?',
+        answers: [
+            { Text: 'Kevin Durant', correct: false },
+            { Text: 'Stephen Curry', correct: false },
+            { Text: 'Russell Westbrook', correct: true },
+            { Text: 'Tim Duncan', correct: false },
+        ]
+    },
+    {
+        question: 'Why was the NBA Season Supended in 2020?',
+        answers: [
+            { Text: 'Summer break', correct: false },
+            { Text: 'Tornado', correct: false },
+            { Text: 'Global Pandemic', correct: true },
+            { Text: 'Too Many Injuries', correct: false },
+        ]
+    },
+    {
+        question: 'What NBA player in history has 0 loses in the Finals?',
+        answers: [
+            { Text: 'Lebron James', correct: false },
+            { Text: 'Kobe Bryant', correct: false },
+            { Text: 'Bill Russell', correct: false },
+            { Text: 'Michael Jordan', correct: true },
+        ]
+    },
+]
